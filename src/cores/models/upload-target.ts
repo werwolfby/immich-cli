22cb07d3d3793ed102760d2ff0c81509aad6b7b7
Alex Tran <alex.tran1502@gmail.com>
2023-02-04 01:50:44 -0600

    chore: optimize constructor of UploadTarget

--- src/cores/models/upload-target.ts
+++ src/cores/models/upload-target.ts
@@ -4,12 +4,12 @@ import { basename } from 'node:path';
 export class UploadTarget {
   public path: string;
 
-  public get id(): string {
-    const stats = fs.statSync(this.path);
-    return `${basename(this.path)}-${stats.size}`.replace(/\s+/g, '');
-  }
+  public id: string;
 
   constructor(path: string) {
     this.path = path;
+
+    const stats = fs.statSync(this.path);
+    this.id = `${basename(this.path)}-${stats.size}`.replace(/\s+/g, '');
   }
 }
