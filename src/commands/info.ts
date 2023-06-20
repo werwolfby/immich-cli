97c7e70c424b76e924b9759ec390c4e1137f8623
Jonathan Jogenfors <jonathan@jogenfors.se>
2023-06-20 16:29:04 +0200

    Rename server-version to info

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,16 +1,13 @@
 import { ServerVersionReponseDto } from 'immich-sdk';
 import { BaseCommand } from '../cli/base-command';
 
-export default class ServerVersion extends BaseCommand<typeof ServerVersion> {
+export default class Info extends BaseCommand<typeof Info> {
   static description = 'Get the server version';
   static enableJsonFlag = true;
 
   public async run(): Promise<ServerVersionReponseDto> {
     this.debug('Getting server version');
     const { data: versionInfo } = await this.immichApi.serverInfoApi.getServerVersion();
-
-    const version = `v${versionInfo.major}.${versionInfo.minor}.${versionInfo.patch}`;
-    this.log(`Connected to Immich ${version} at ${this.flags.server}`);
 
     return versionInfo;
   }
